02496a68834d30ccb9e2ca89d1fbc418d96a0659
manassesqueiroz <manassescruzgt@gmail.com>
2024-03-03 17:06:34 -0300

    adc notification

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { supabaseServer } from "@/lib/supabase/server";
 export default async function Page() {
   return (
   <div className="max-w-3xl mx-auto md:py-10 h-screen">
-    <div className="flex flex-col h-full border rounded-md boder">
+    <div className="flex flex-col h-full border rounded-md boder relative ">
       <ChatHeader />
       <ChatMessage />
       <ChatInput />
